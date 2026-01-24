1177f7972773868cca99916bdde246d1e3183c49
hot-gamja <coke0701@gmail.com>
2026-01-24 19:31:13 +0900

    0124

--- src/main/java/com/example/dao/UserDao.java
+++ src/main/java/com/example/dao/UserDao.java
@@ -1,55 +1,70 @@
 package com.example.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.example.model.User;
-import java.sql.*;
 
 /**
  * User Data Access Object
  */
 public class UserDao {
-    // PostgreSQL JDBC 드라이버와 연결 정보 (환경에 맞게 수정)
-    private static final String DRIVER = "org.postgresql.Driver";
-    private static final String URL = "jdbc:postgresql://localhost:5432/spring-framework-db";
-    private static final String USER = "user";
-    private static final String PASSWORD = "password";
+        // PostgreSQL JDBC 드라이버와 연결 정보 (환경에 맞게 수정)
+        private static final String DRIVER = "org.postgresql.Driver";
+        private static final String URL = "jdbc:postgresql://localhost:5432/spring-framework-db";
+        private static final String USER = "user";
+        private static final String PASSWORD = "password";
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/spring-framework-db", "user", "password");
+        public static void main(String[] args) {
+                UserDao dao = new UserDao();
+                User user = new User();
+                user.setId("whiteshp");
+                user.setName("back");
+                user.setPassword("123");
+                try {
+                        dao.add(user);
+                        System.out.println(user.getId() + " 등록성공");
+                        User user2 = dao.get(user.getId());
+                        System.out.println(user2.getName());
+                        System.out.println(user2.getName());
+                        System.out.println(user2.getPassword());
+                        System.out.println(user2.getId() + "조회성공");
+                } catch (ClassNotFoundException | SQLException e) {
+                        e.printStackTrace();
+                }
+        }
 
-        PreparedStatement ps = c.prepareStatement(
-                "INSERT INTO \"user\" (\"userid\", \"name\", \"password\") VALUES (?, ?, ?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
+        public void add(User user) throws ClassNotFoundException, SQLException {
+                Class.forName(DRIVER);
+                Connection c = DriverManager.getConnection(URL, USER, PASSWORD);
+                PreparedStatement ps = c.prepareStatement(
+                                "INSERT INTO \"user\" (\"userid\", \"name\", \"password\") VALUES (?, ?, ?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+                ps.executeUpdate();
+                ps.close();
+                c.close();
+        }
 
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
-    }
-
-    public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/spring-framework-db", "user", "password");
-
-        PreparedStatement ps = c.prepareStatement(
-                "SELECT * FROM \"user\" WHERE \"userid\" = ?");
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        User user = new User();
-        user.setId(rs.getString("userid"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return user;
-    }
+        public User get(String id) throws ClassNotFoundException, SQLException {
+                Class.forName(DRIVER);
+                Connection c = DriverManager.getConnection(URL, USER, PASSWORD);
+                PreparedStatement ps = c.prepareStatement(
+                                "SELECT * FROM \"user\" WHERE \"userid\" = ?");
+                ps.setString(1, id);
+                ResultSet rs = ps.executeQuery();
+                rs.next();
+                User user = new User();
+                user.setId(rs.getString("userid"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+                rs.close();
+                ps.close();
+                c.close();
+                return user;
+        }
 }
--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -1,32 +1,12 @@
 package com.example.model;
+import lombok.Data;
 
 // 예시 User 도메인 클래스입니다. 실제 프로젝트에 맞게 수정하세요.
+@Data
 public class User {
     String id;
     String name;
     String password;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+ 
 }
